10399649df6600971c046c702b882c011a57389d
gregglickert <gglick9@gmail.com>
2022-02-01 12:17:56 -0600

    update

---
+++ HPC_Tutorial/README.md
@@ -0,0 +1,7 @@
+# HPC tutorials
+## To build the network first make sure your enviorment is activate.
+Then run the command
+```
+sbatch batch_build.sh
+```
+This will build the network and send an email saying the network is build. MAKE SURE TO CHANGE TO EMAIL IN THE BATCH FILES SO I DON'T GET A BUNCH OF EMAILS 
--- HPC_Tutorial/batch_run.sh
+++ HPC_Tutorial/batch_run.sh
@@ -4,13 +4,13 @@
 #SBATCH -n 50
 #SBATCH --qos=normal
 #SBATCH --job-name=HPC_tut
-#SBATCH --output=HPC_tut.out
+#SBATCH --output=HPC_run.out
 #SBATCH --time 0-00:30
 
 START=$(date)
 mpiexec nrniv -mpi -quiet -python run_network.py
 END=$(date)
 
-{ printf "Start: $START \nEnd:   $END\n" & python plot.py & sleep 20; }| mail -r gregglickert@mail.missouri.edu -s "HPC tutorial result" -a raster.png gregglickert@mail.missouri.edu
+{ printf "Start: $START \nEnd:   $END\n" & python plot.py & sleep 20 & printf "\n\n" & git diff components/synaptic_models/; }| mail -r gregglickert@mail.missouri.edu -s "HPC tutorial result" -a raster.png gregglickert@mail.missouri.edu
 
 echo "Done running model at $(date)"
