7951e9fb8925c6974bbadb986c13a030d6d62804
Greg Glickert <59316745+GregGlickert@users.noreply.github.com>
2023-02-21 13:26:05 -0600

    Update batch_run.sh

--- HPC_Tutorial/batch_run.sh
+++ HPC_Tutorial/batch_run.sh
@@ -11,6 +11,4 @@ START=$(date)
 mpiexec nrniv -mpi -quiet -python run_network.py
 END=$(date)
 
-{ printf "Start: $START \nEnd:   $END\n" & python plot.py & sleep 20 & printf "\n\n" & git diff components/synaptic_models/; }| mail -r gregglickert@mail.missouri.edu -s "HPC tutorial result" -a raster.png gregglickert@mail.missouri.edu
-
 echo "Done running model at $(date)"
